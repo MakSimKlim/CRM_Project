2a1566c66ba14c9e310cebd2cfe3f00a43e5c1e8
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-04-24 21:15:21 +0500

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <time.h>
+#include <string>
+
+using namespace std;
+
+class User //заготовка под родительский класс пользователей
+{
+public:
+	//конструктор 
+	User(int setId, int setPosition, string setLastName, string setFirstName, string setPatronymic)
+	{
+		this->id;
+		this->position;
+		
+		
+		this->lastname;    // фамилия
+		this->firstname;   // имя
+		this->patronymic;  // отчество
+	}
+
+	User() {}
+
+	//методы класса User:
+
+	////получить данные о пользователе
+	//void getUser(int getId)
+	//{
+	//
+	//}
+
+
+private:
+	int id{};
+	int position{};       // должность
+
+	string lastname;      //фамилия
+	string firstname;     // имя
+	string patronymic;    // отчество
+};
+
+
